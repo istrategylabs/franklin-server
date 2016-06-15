638c352419637c93ec4a338e8dc9f6a9a37ea7be
Jeremy Carbaugh <jeremy@jcarbaugh.com>
2016-06-15 11:08:27 -0400

    Support for custom 404 pages (#4)
    New 404 pages and support for custom 404 pages

--- web.py
+++ web.py
@@ -1,9 +1,11 @@
 import datetime
 import hmac
 import logging
+import os
 from base64 import b64encode
 from urllib.parse import quote, urljoin
 
+import aiofiles
 import aiohttp
 from aiohttp import web
 from cachetools import TTLCache
@@ -12,7 +14,7 @@ from decouple import config
 from util import filter_headers
 
 
-__version__ = '1.0.0'
+__version__ = '2.0.0'
 
 
 #
@@ -31,6 +33,9 @@ HOST_CACHE_SIZE = config('HOST_CACHE_SIZE', cast=int, default=128)
 
 PROXY_REQUEST_HEADERS = ('Cache-Control', 'If-Modified-Since', 'If-None-Match')
 PROXY_RESPONSE_HEADERS = ('Content-Length', 'Last-Modified', 'ETag')
+DEFAULT_RESPONSE_HEADERS = {
+    'Server': 'franklin-server/{}'.format(__version__),
+}
 
 A_YEAR = 60 * 60 * 24 * 365
 
@@ -149,6 +154,44 @@ async def fetch_s3(bucket, path, method='GET', headers=None, signed=True):
     return resource
 
 
+async def handle_404(host_config):
+    """ Try to fetch a custom 404 page from S3. If the page exists, return it
+        as expected. If it does not exist, return a default 404 page instead.
+        The presence or lack of a custom 404 will be saved on the host_config
+        and stored until it is evicted from the cache, but the custom file
+        itself is not yet cached.
+    """
+
+    try_custom_404 = host_config.get('custom_404', True)
+
+    if try_custom_404:
+
+        path = '{}/404.html'.format(host_config['path'])
+        resource = await fetch_s3(AWS_BUCKET, path)
+
+        has_custom_404 = resource['status'] == 200
+        host_config['custom_404'] = has_custom_404
+
+        if has_custom_404:
+            return web.Response(body=resource['data'],
+                                content_type='text/html',
+                                headers=DEFAULT_RESPONSE_HEADERS,
+                                status=404)
+
+    # render default 404
+
+    path = os.path.join(os.path.dirname(__file__),
+                        'templates',
+                        '404-file_not_found.html')
+    async with aiofiles.open(path) as fp:
+        content = await fp.read()
+
+    return web.Response(text=content,
+                        content_type='text/html',
+                        headers=DEFAULT_RESPONSE_HEADERS,
+                        status=404)
+
+
 async def request_handler(request):
     """ Handle all requests and return the response,
         either the proxied object or an appropriate error.
@@ -158,8 +201,16 @@ async def request_handler(request):
     host_config = await resolve_host_config(hostname)
 
     if not host_config:
-        return web.Response(text='Host not found',
-                            content_type='text/plain',
+
+        path = os.path.join(os.path.dirname(__file__),
+                            'templates',
+                            '404-host_not_found.html')
+        async with aiofiles.open(path) as fp:
+            content = await fp.read()
+
+        return web.Response(text=content,
+                            content_type='text/html',
+                            headers=DEFAULT_RESPONSE_HEADERS,
                             status=404)
 
     resource_path = request.match_info.get('resource_path', '')
@@ -175,12 +226,12 @@ async def request_handler(request):
         return web.Response(status=304)
 
     if resource['status'] != 200:
-        return web.Response(body=resource['data'],
-                            content_type='text/html',
-                            status=404)
+        resp = await handle_404(host_config)
+        return resp
 
     response_headers = filter_headers(
         resource['headers'], PROXY_RESPONSE_HEADERS)
+    response_headers.update(DEFAULT_RESPONSE_HEADERS)
 
     max_age = CACHE_MAX_AGES.get(resource['headers']['Content-Type'])
     response_headers['Cache-Control'] = \
